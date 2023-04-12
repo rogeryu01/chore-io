1a4f9c54397a1db844fcdb55257cb4889b13abd6
rogeryu01 <59711432+rogeryu01@users.noreply.github.com>
2023-04-11 22:35:16 -0400

    hello

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Chore {
+  name: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'choreio-2.0';
+  newChoreName = '';
+  newChoreDescription = '';
+  newChorePerson ='';
+  chores: Chore[] = [];
+
+  onSubmit() {
+    const newChore: Chore = {
+      name: this.newChoreName,
+      description: this.newChoreDescription,
+  
+    };
+
+    this.chores.push(newChore);
+    this.newChoreName = '';
+    this.newChoreDescription = '';
+    this.newChorePerson = '';
+  }
+
+  deleteChore(chore: Chore) {
+    const index = this.chores.indexOf(chore);
+    this.chores.splice(index, 1);
+  }
 }
