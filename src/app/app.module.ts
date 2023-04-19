174726b382c3164bb0bdc5955294bc6830d9d700
Jeff Yi <jeffyi.kyu@gmail.com>
2023-04-18 20:02:26 -0400

    add initial backend file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
+import { ChoreListComponent } from './chore-list/chore-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChoreListComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
