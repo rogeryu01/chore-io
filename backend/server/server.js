afd0c4552c58e94fc40ff872077b2c7a3478b52d
Jeff Yi <jeffyi.kyu@gmail.com>
2023-04-18 23:18:14 -0400

    testing insomnia

--- backend/server/server.js
+++ backend/server/server.js
@@ -13,12 +13,48 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const choreSchema = new mongoose.Schema({
-    name: String,
-    assignedTo: String,
-    dueDate: Date
+    name: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: String,
+        required: true
+    },
+    assignedTo: {
+        type: String,
+        required: true
+    },
+    completionStatus: {
+        type: String,
+        required: true
+    },
+    accepted: {
+        type: Boolean,
+        default: false
+    },
+    choreId: {
+        type: Number,
+        required: true
+    },
+    assignedDate: {
+        type: Date
+    },
+    dueDate: {
+        type: Date,
+        required: true
+    },
+    repeatFor: {
+        type: String,
+        required: true
+    },
+    points: {
+        type: Number,
+        required: true
+    }
 });
 
-// app.use('/api', routes)
+//app.use('/api', routes)
 
 const Chore = mongoose.model('Chore', choreSchema);
 
